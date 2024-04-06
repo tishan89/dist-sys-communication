48687e513f6f4983a26d3a7de0ba37682dcf23e5
tishan89 <dtishanpubudu@gmail.com>
2024-04-06 21:12:36 +0530

    Adds the name server implementation

--- client/src/main/java/ds/tutorial/communication/client/CheckBalanceServiceClient.java
+++ client/src/main/java/ds/tutorial/communication/client/CheckBalanceServiceClient.java
@@ -1,38 +1,42 @@
 package ds.tutorial.communication.client;
 
+import java.io.IOException;
 import java.util.Scanner;
+
+import io.grpc.ConnectivityState;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import ds.tutorial.communication.grpc.generated.*;
+import ds.tutorial.naming.client.NameServiceClient;
 
 public class CheckBalanceServiceClient {
+    public static final String NAME_SERVICE_ADDRESS = "http://localhost:2379";
     private ManagedChannel channel = null;
     BalanceServiceGrpc.BalanceServiceBlockingStub clientStub = null;
     String host = null;
     int port = -1;
 
-    public static void main(String[] args) throws InterruptedException {
-        String host = null;
-        int port = -1;
-        if (args.length != 2) {
-            System.out.println("Usage CheckBalanceServiceClient <host> <port>");
-            System.exit(1);
-        }
-        host = args[0];
-        port = Integer.parseInt(args[1].trim());
-
-
-        CheckBalanceServiceClient client = new CheckBalanceServiceClient(host, port);
+    public static void main(String[] args) throws InterruptedException, IOException {
+        
+        CheckBalanceServiceClient client = new CheckBalanceServiceClient();
         client.initializeConnection();
         client.processUserRequests();
         client.closeConnection();
 
     }
 
-    public CheckBalanceServiceClient (String host, int port) {
-        this.host = host;
-        this.port = port;
+    public CheckBalanceServiceClient () throws IOException, InterruptedException {
+        fetchServerDetails();
+
     }
+    
+    private void fetchServerDetails() throws IOException, InterruptedException {
+       NameServiceClient client = new NameServiceClient(NAME_SERVICE_ADDRESS);
+       NameServiceClient.ServiceDetails serviceDetails = client.findService("CheckBalanceService");
+       host = serviceDetails.getIPAddress();
+       port = serviceDetails.getPort();
+   }
+
 
     private void initializeConnection () {
         System.out.println("Initializing Connecting to server at " + host + ":" + port);
@@ -40,13 +44,14 @@ public class CheckBalanceServiceClient {
                 .usePlaintext()
                 .build();
         clientStub = BalanceServiceGrpc.newBlockingStub(channel);
+        channel.getState(true);
     }
 
     private void closeConnection() {
         channel.shutdown();
     }
 
-    private void processUserRequests() throws InterruptedException {
+    private void processUserRequests() throws InterruptedException, IOException {
 
         while (true) {
             Scanner userInput = new Scanner(System.in);
@@ -58,6 +63,14 @@ public class CheckBalanceServiceClient {
                     .newBuilder()
                     .setAccountId(accountId)
                     .build();
+            ConnectivityState state = channel.getState(true);
+            while (state != ConnectivityState.READY) {
+               System.out.println("Service unavailable, looking for a service provider..");
+               fetchServerDetails();
+               initializeConnection();
+               Thread.sleep(5000);
+               state = channel.getState(true);
+           }
 
             CheckBalanceResponse response = clientStub.checkBalance(request);
             System.out.printf("My balance is " + response.getBalance() + " LKR");
--- server/src/main/java/ds/tutorial/communication/server/BankServer.java
+++ server/src/main/java/ds/tutorial/communication/server/BankServer.java
@@ -1,11 +1,13 @@
 package ds.tutorial.communication.server;
 
+import ds.tutorial.naming.client.NameServiceClient;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class BankServer {
+    public static final String NAME_SERVICE_ADDRESS = "http://localhost:2379";
     public static void main (String[] args) throws Exception{
-        int serverPort = 11436;
+        int serverPort = 1143;
 
         Server server = ServerBuilder
                 .forPort(serverPort)
@@ -13,6 +15,9 @@ public class BankServer {
                 .build();
 
         server.start();
+        NameServiceClient client = new NameServiceClient(NAME_SERVICE_ADDRESS);
+        client.registerService("CheckBalanceService", "127.0.0.1", serverPort, "tcp");
+
         System.out.println("BankServer Started and ready to accept requests on port " + serverPort);
 
         server.awaitTermination();
